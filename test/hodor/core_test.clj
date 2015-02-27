0097c99324e44fdf3e487cf73a416a7827b323d5
Russell Mull <russell.mull@puppetlabs.com>
2015-02-27 15:29:12 -0800

    Add basic tests

--- test/hodor/core_test.clj
+++ test/hodor/core_test.clj
@@ -4,7 +4,38 @@
             [hodor.core :refer :all]))
 
 (deftest arithmetic
-  (testing "simple addition"
+  (testing "immediates"
     (are [exp val] (= (compile-and-run exp) val)
-         '(+ 10 15) "25"
-         '(+ -10 15) "5")))
+         0 "0"
+         42 "42"
+         \a "a"
+         \Z "Z"
+         :empty-list "()"
+         'false "false"
+         'true "true"))
+
+  (testing "unary primitives"
+    (are [exp val] (= (compile-and-run exp) val)
+         '(zero? 0) "true"
+         '(zero? 1) "false"
+         '(zero? \a) "false"
+         '(zero? false) "false"
+         '(zero? :empty-list) "false"
+
+         '(null? :empty-list) "true"
+         '(null? 0) "false"
+         '(null? \a) "false"
+         '(null? false) "false"
+
+         '(integer? 0) "true"
+         '(integer? 42) "true"
+         '(integer? \a) "false"
+         '(integer? false) "false"
+         '(integer? :empty-list) "false"
+
+         '(boolean? true) "true"
+         '(boolean? false) "true"
+         '(boolean? 42) "false"
+         '(boolean? \a) "false"
+         '(boolean? :empty-list) "false"
+         )))
